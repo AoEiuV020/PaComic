ccf35bb1609204d0f68abe2a54cd40e81c25deec
AoEiuV020 <aoeiuv020@gmail.com>
2017-06-11 22:09:54 +0800

    爬下dm5漫画页面
    顺便之前popomh漫画页面地址索引0开始要+1,

--- api/src/main/kotlin/cc/aoeiuv020/comic/api/dm5/Dm5ComicContentsSpider.kt
+++ api/src/main/kotlin/cc/aoeiuv020/comic/api/dm5/Dm5ComicContentsSpider.kt
@@ -8,8 +8,10 @@ import org.jsoup.nodes.Element
  * Created by AoEiuV020 on 17-6-3.
  */
 class Dm5ComicContentsSpider(dm5: Dm5, element: Element) : ComicContentsSpider() {
-    override val name: String = element.attr("title")
-    val comicPageUrl = dm5.home + element.attr("href")
-    override val comicPage: ComicPageSpider
-        get() = TODO("not implemented") //To change initializer of created properties use File | Settings | File Templates.
+    override val name: String = element.select("a").attr("title")
+    val comicPageUrl = dm5.home + element.select("a").attr("href")
+    override val comicPage: ComicPageSpider by lazy {
+        val pageCount = element.text().replace(Regex(".*(\\d).*"), "$1").toInt()
+        Dm5ComicPageSpider(dm5, comicPageUrl, pageCount)
+    }
 }
