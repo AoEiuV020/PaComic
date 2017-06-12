a5c8a8deeb59f8fb4625711413aee766ab0fc254
AoEiuV020 <aoeiuv020@gmail.com>
2017-06-12 16:43:12 +0800

    Change pagesCount to pageCount
    并不清楚哪个对，看不顺眼就改了，

--- api/src/main/kotlin/cc/aoeiuv020/comic/api/ComicPageSpider.kt
+++ api/src/main/kotlin/cc/aoeiuv020/comic/api/ComicPageSpider.kt
@@ -4,6 +4,6 @@ package cc.aoeiuv020.comic.api
  * Created by AoEiuV020 on 17-5-31.
  */
 abstract class ComicPageSpider : Spider() {
-    abstract val pagesCount: Int
+    abstract val pageCount: Int
     abstract fun imgUrl(i: Int): String
 }
