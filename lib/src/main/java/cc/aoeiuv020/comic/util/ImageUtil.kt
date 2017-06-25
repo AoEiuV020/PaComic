6ecc4dfd86ed1a2091d071cbdedbc005e4293465
AoEiuV020 <aoeiuv020@gmail.com>
2017-06-25 17:46:41 +0800

    添加glide做图片缓存

--- lib/src/main/java/cc/aoeiuv020/comic/util/ImageUtil.kt
+++ lib/src/main/java/cc/aoeiuv020/comic/util/ImageUtil.kt
@@ -1,28 +1,16 @@
 package cc.aoeiuv020.comic.util
 
-import android.graphics.BitmapFactory
 import android.widget.ImageView
-import cc.aoeiuv020.comic.manager.R
+import com.bumptech.glide.Glide
 import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.debug
-import org.jetbrains.anko.doAsync
-import org.jetbrains.anko.uiThread
-import java.net.URL
 
 /**
  * Created by AoEiuV020 on 17-5-29.
  */
 object ImageUtil : AnkoLogger {
-    fun asyncSetImageUrl(image: ImageView, url: String) = with(image) {
+    fun asyncSetImageUrl(image: ImageView, url: String) {
         debug { "${image.hashCode()}, $url" }
-        setTag(R.id.asyncSetImageUrl, url)
-        doAsync {
-            val bit = BitmapFactory.decodeStream(URL(url).openConnection().getInputStream())
-            uiThread {
-                if (getTag(R.id.asyncSetImageUrl) == url && bit != null) {
-                    setImageBitmap(bit)
-                }
-            }
-        }
+        Glide.with(image).load(url).into(image)
     }
 }
