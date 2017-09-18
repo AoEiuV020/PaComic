d642c3e03981e9d20a18be1592ebd6afcd357ab0
AoEiuV020 <aoeiuv020@gmail.com>
2017-09-18 22:57:00 +0800

    解决动漫屋部分本地漫画解析失败的问题
    因为jsoup selector太严格，

--- app/src/main/java/cc/aoeiuv020/comic/api/dm5.kt
+++ app/src/main/java/cc/aoeiuv020/comic/api/dm5.kt
@@ -90,7 +90,7 @@ class Dm5Context : ComicContext() {
     override fun getComicPages(comicIssue: ComicIssue): List<ComicPage> {
         val cid = comicIssue.url.replace(Regex(".*/m(\\d*)/"), "$1")
         val first = getHtml(comicIssue.url)
-        val chapter = first.select("body > div.viewBar > div:nth-child(9) > div#chapterpager").firstOrNull() ?: return emptyList()
+        val chapter = first.select("div#chapterpager").firstOrNull() ?: return emptyList()
         val pagesCount = chapter.select("a:nth-last-child(1)").firstOrNull()?.run { text().toInt() } ?: 1
         return List(pagesCount) {
             ComicPage(absUrl("/m$cid-p${it + 1}/"))
