b6e633740a1dab9328582f9623db3e57bca15e33
AoEiuV020 <aoeiuv020@gmail.com>
2017-09-20 15:38:33 +0800

    删除两行不小心加入的没用的代码
    捂脸，随手试试的代码忘了删，还提交了，

--- app/src/main/java/cc/aoeiuv020/comic/ui/page.kt
+++ app/src/main/java/cc/aoeiuv020/comic/ui/page.kt
@@ -7,7 +7,6 @@ import android.support.v4.view.PagerAdapter
 import android.support.v4.view.ViewPager
 import android.view.View
 import android.view.ViewGroup
-import android.webkit.WebView
 import android.widget.SeekBar
 import cc.aoeiuv020.comic.R
 import cc.aoeiuv020.comic.api.ComicIssue
@@ -131,8 +130,6 @@ class ComicPageAdapter(val ctx: Context, private val presenter: ComicPagePresent
             root.progressBar.visibility = View.GONE
         })
         container.addView(root)
-        val w = WebView(ctx)
-        w.getContentHeight()
         return root
     }
 
